2b449d9bea622bab6e62ba63680a4ebdb0c86008
Kamzu <info@kamzu.net>
2025-03-05 22:37:11 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,18 @@
+# Podstawowe informacje o bocie
 TOKEN = "token"
+
+# Konfiguracja ID serwera
 GUILD_ID = 892087879063457852
+
+# ID kanału wejścia i wyjścia
 WELCOME_CHANNEL_ID = 1345355220204519485
 LEAVE_CHANNEL_ID = 1345355288248713287
+
+# ID kanałów z logami
+LOG_CHANNEL_ID = 1324841027231547534
+
+# ==========================================
+# - Wartości oznaczone jako ID muszą być liczbami (int).
+# - Nigdy nie udostępniaj tokena bota publicznie!
+# - Aby pobrać ID kanału/roli/użytkownika, włącz tryb dewelopera w Discord.
+# ==========================================
